0918bc48bf8055a41a2b30d3a4878f62ae73c84a
silviu.avram <silviu.avram@PC311670>
2023-01-18 00:33:49 +0200

    Added React Router

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,158 +1,103 @@
-import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, MenuItem, Switch, Toolbar, Tooltip, Typography } from "@mui/material";
-import KitchenIcon from '@mui/icons-material/Kitchen';
-import MenuIcon from '@mui/icons-material/Menu';
+import {
+  AppBar,
+  Badge,
+  Box,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import KitchenIcon from "@mui/icons-material/Kitchen";
+import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
-import AdbIcon from '@mui/icons-material/Adb';
+// import AdbIcon from '@mui/icons-material/Adb';
+import { NavLink } from "react-router-dom";
+import { ShoppingCart } from "@mui/icons-material";
 
-
-const pages = ['Products', 'About Us', 'Contact'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const midLinks = [
+  { title: "products", path: "/products" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
 
 interface Props {
-    darkMode: boolean;
-    handleThemeChange: () => void;
+  darkMode: boolean;
+  handleThemeChange: () => void;
 }
 
-export default function Header({darkMode, handleThemeChange}: Props) {
+const navStyles = {
+  color: "inherit",
+  textDecoration: "none",
+  typography: "h6",
+  "&:hover": {
+    color: "gray.500",
+  },
+  "&:active": {
+    color: "text.primary",
+  },
+};
 
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-  
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
-
-    return (
-        <AppBar position="static" sx={{mb: 4}}>
-            <Container maxWidth="xl">
-            <Toolbar disableGutters>
-                <KitchenIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                <Typography
-                    variant="h6"
-                    sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    fontWeight: 700,
-                    letterSpacing: '.3rem',
-                    color: 'inherit',
-                    textDecoration: 'none',
-                    }}
-                >
-                Kitchen Deal
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-                >
-                    <MenuIcon />
-                </IconButton>
-            <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Switch checked={darkMode} onChange={handleThemeChange}/>
-
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+export default function Header({ darkMode, handleThemeChange }: Props) {
+  return (
+    <AppBar position="static" sx={{ mb: 4 }}>
+      <Container maxWidth="xl">
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box display="flex" alignItems="center">
+            <KitchenIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Typography
+              component={NavLink}
+              to="/"
+              exact
+              sx={navStyles}
+              variant="h6"
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+              Kitchen Deal
+            </Typography>
+            <Switch checked={darkMode} onChange={handleThemeChange} />
           </Box>
 
-            </Toolbar>
-            </Container>
-        </AppBar>
-    )
+          <List sx={{ display: "flex" }}>
+            {midLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+
+          <Box display="flex" alignItems="center">
+            <List sx={{ display: "flex" }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+            <IconButton size="large" sx={{ color: "inherit" }}>
+              <Badge badgeContent={4} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -12,18 +12,18 @@ export default function Catalog() {
       .then(data => setProducts(data))
     }, []);
 
-    const addProduct = () => {
-      setProducts(prevState => [...prevState, 
-        {
-          id: prevState.length + 101,
-          name: 'product'+ (prevState.length + 1), 
-          price: (prevState.length * 100) + 100 ,
-          brand: 'some brand',
-          description: 'some description',
-          pictureUrl: 'http://picsum.photos/' + (prevState.length * 100) + 1
-      }
-      ])
-    }
+    // const addProduct = () => {
+    //   setProducts(prevState => [...prevState, 
+    //     {
+    //       id: prevState.length + 101,
+    //       name: 'product'+ (prevState.length + 1), 
+    //       price: (prevState.length * 100) + 100 ,
+    //       brand: 'some brand',
+    //       description: 'some description',
+    //       pictureUrl: 'http://picsum.photos/' + (prevState.length * 100) + 1
+    //   }
+    //   ])
+    // }
 
     return (
       <>
